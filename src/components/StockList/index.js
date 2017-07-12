42fd4bbd110b45c7c61978bea3917b281e37f1e6
claraLws <clara.css27@gmail.com>
2017-07-12 17:14:14 +0800

    Revise StockList export function

--- src/components/StockList/index.js
+++ src/components/StockList/index.js
@@ -47,13 +47,13 @@ class StockList extends React.PureComponent {
   }
 
   convertObjectsToString(objArray) {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
     var str = '';
 
     for (var i = 0; i < array.length; i++) {
       var line = '';
       for (var index in array[i]) {
-        if (line != '') line += ','
+        if (line !== '') line += ','
 
         line += array[i][index];
       }
